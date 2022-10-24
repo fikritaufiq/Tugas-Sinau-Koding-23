74643323bdc03b778e838a266d30d484fc22907b
Fikri Taufiq Rahman <85074553+fikritaufiq@users.noreply.github.com>
2022-10-24 21:58:19 +0700

    Add files via upload

---
+++ Tugas2.java
@@ -0,0 +1,13 @@
+public class Tugas2 {
+    public static void main(String[] args) {
+        for(int x = 1; x<=10;x++){
+            if (x%2 == 1){
+                System.out.println("Angka " + x + " ini adalah Ganjil");
+            }
+            else{
+                System.out.println("Angka " + x + " Ini adalah Genap");
+            }
+        }
+    }
+}
+
